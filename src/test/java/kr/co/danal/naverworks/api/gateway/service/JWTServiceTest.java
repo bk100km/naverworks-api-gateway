79f0b60bdb9aa5401e089909f1ecf6f1c7905881
bk100km <bk100km@naver.com>
2024-06-27 10:17:10 +0900

    (feat) [RDN-4] Naverworks API Gateway 개발
    - log level, file path 수정
    - channel file path 수정

--- src/test/java/kr/co/danal/naverworks/api/gateway/service/JWTServiceTest.java
+++ src/test/java/kr/co/danal/naverworks/api/gateway/service/JWTServiceTest.java
@@ -7,58 +7,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
 @SpringBootTest
 public class JWTServiceTest {
 
 	@Autowired
 	JWTService jwtService;
 
-	@Test
-	public void getTokenAuto() throws IOException, GeneralSecurityException {
+//	@Test
+	public void getTokenAuto() {
 		String token = jwtService.getServerToken();
 		assertThat(token).isNotNull();
 	}
 
-	@Test
-	public void getTokenCustom() throws IOException, GeneralSecurityException {
-		String serverPrivateKey = "-----BEGIN PRIVATE KEY-----" +
-				"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDQOfAdsmBCQbEK" +
-				"VOwOjHSLzw7f7BT1iMNjIk7FPHSxCgOvfIZC9f0rkPk9H6EchgPkFrwGuSDOkdnw" +
-				"//ARjgGg9gyo/2b3g6wS5ilYb67VDLuP8eXH4jDIKEs+NpQZM7JHarwNhVbHdUuX" +
-				"6nd1CzWePp/wDC9EMJWjVpFBuiAKlStgYkG5pCA0IP6VtayrcCZ7aIuK5fRRXqrW" +
-				"fEBYwfx+M9D/miyLyOcfqM4gW0cI+vsNCzgoEXfp+zbHPtutNqhdC332kSHNctRM" +
-				"9besBkOFTakWrWxQSBd/2cGhxFrf90zMmrHPBqkFjA2ubNrtCYDRoFC3SkWOLz9z" +
-				"3KMiCGFtAgMBAAECggEAUk5khEiK1gXDZjMVL9cDBnRtC8P0sO1DVj8sZbg04/ny" +
-				"ZP5Sz8sOLJdcfXWwKGcsyI7X+cxZlG2AeUuhaD76k9ZOQMrbt4CblsjVlPhoYhxr" +
-				"U0Wpry3QXH3Y6BmIxsFxedtNxAZtu9+4Zq8uUtyAcDGO5MLcOBcAPxUry5A3tBZ2" +
-				"DX6ZLRmehMQKTFORHVV+XhXwBiR2hOuOACXxgkU3lpcaLaohxsSfTILPTgmJjOaO" +
-				"mPIjKhN3LIGYEXP01mhI8tM+ttzXEFov9iuQETv7/1mj8YDds6veQLy31oc8I9fg" +
-				"y9mFs3ZMV/WJZtKM4OFYaQpjKq7fG5ha/rkBbbCA3QKBgQDnSAa//r8O1q+QQz/M" +
-				"NlF9smC6hy4Ce1AHqe7j3/MTRtlxawISNBsWGxYTavXofFZ8afiDpp8G9S4d7HT1" +
-				"jntDfp1ZtHpb9MWVitgUBSXTD3Iqx+/EilnyyWe+5fje5hKfirltsOmFYe2zbSsc" +
-				"HbfHZoGplnQm57fgAnihuIyizwKBgQDmex1FgrJG2piuwr56N3LMwsgsccXA58Ft" +
-				"leC9wrrzQi1aILOpAPpam3bfgHLInIMtONJUuYNErJLxteXfapE5D0lGhNaRiaVT" +
-				"jAjsd2RV50eaOTqiYcwrP648xQi30oWsGpM15a+lSrgK+dGD9OtFolBtgQy7LkD+" +
-				"qkhnIhg3AwKBgGsQOp+yS7uQwmDj4KqmEn8/jeuLHE9lyhxiHAY864dm6s3IHYNT" +
-				"v54yF7wRbZibxmV9oN9AyKkt4pVTmqj5tjH+cNTP6TycSML7LR/HakUUB2OqfmHb" +
-				"MdxkQ7RpPRXiJ4jXbRNMLC3ksWa4CJbRYvvZhfh9f3NrHQGD41fp2KCdAoGBAJcB" +
-				"LopuONOHQOWvHhEA5ywUqgXEfOYIcYiU2+w3+ziJsSENr+gYhe57UQTaKVoagS9N" +
-				"vTBZYxHJZ3UJ0hPwxDj4NPAI9HPiFPmKFAapqolFwHng6POe05+6VzPle/LD8WRy" +
-				"/OQhGlVXgQFf789e1g6Ha95y4J56jYbMlidMBUV1AoGAevaEE08LZ6PODqLss6dx" +
-				"JN+VZE7cGXFhAI/5EdR0sN2P4wH9aWm9VGGAmqBgqPNaGTkGCDl+VZdNeeOCVODf" +
-				"GRBQsblx7peQzwiegdn00foMlQg1ltOXgSwJeg7CRKs+a2qg4isjQwThH6x5LFhv" +
-				"hpsTot4zNTugMvX4LRmCsO8=" +
-				"-----END PRIVATE KEY-----";
+//	@Test
+	public void getTokenCustom() {
+		String serverPrivateKey = "-----BEGIN PRIVATE KEY-----\n" +
+				"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrdJNJKh5AGV9M\n" +
+				"Jr1xiKKxaenG/ARymXxFdpWi+Mz0pGJsat3gdJeE07m4S3zWbZ0m2E5CifJ8mZwU\n" +
+				"6KsPzcKtqxMDn6T66119S9fWXTBy/ypTsjSbyhecpk9T1T55YZ7WwyKUjAw3jQFJ\n" +
+				"fP4i0yQEWvRR9bs3aieHhhTbl/FhWSKDAAfZKvL3BmybO9rsObUSqWlN2q86o8YR\n" +
+				"oDVZVQvHf/83pBzyLP2INRG8XQScFGnAsHnu2l2hQR1u5SJS92s5ZnJDR+UmyxhT\n" +
+				"l8aLPxx5wDp/hDs3YhGo89/EWL7xd1FZieRx8NXPezReIloRprJIbNOUvE0OE+mC\n" +
+				"ktHAtJD7AgMBAAECggEAFztEebVrQ+QsNXVKVgfdg4yGUJ/1OlHxtLfMrrhnjP55\n" +
+				"F5ejWLZzkVve3kncMHHJyWmlsP5gDOIDdBHSm+GVb1Ku0N7gy974W/Ha3QDjf7Fz\n" +
+				"WsPavQE1EdS3CvTp8ih4DUGDff076osAkaSOCsnxJ2XrlO9m3qtTj9lrGhYMbtQb\n" +
+				"w1ycfzYVmJviitaSuf0bsFluXce3MWCkG3udM11XkT3A0vybZvzOL40JDo4MYj35\n" +
+				"nQHdzJBakaLMqHs/NReK3WuNUXBgKVw/FSaTqBVCczXFeBqvF7cVA5PILRmlgwt8\n" +
+				"4xtPRYsAdpAgvoVk3MpfwWQk+Qu5enHv7nYjFRpZAQKBgQDvHj1tUzSMn51uOtLN\n" +
+				"uKYDxHfDK/e9aii7RtJg42HCqWsuMYuslUoGrUHWDfvxuQpML8H31kPAnX7WpYPR\n" +
+				"zQvhYw7IPz9ya69ujhT20OrVg29LJHWL+fWOPvK/X8pVjyayuUZ6RqQB0pZqdq/S\n" +
+				"UheOPnMr6NzS/uCEfa7ODpC6GQKBgQC3j2qBvFEhMJ8Z28P9gJzBD8asYdOUYKEr\n" +
+				"MTZJMHULz4N1rDk8Iv4HkNMob3FdJD1vWqIWqtyE+MO1oBmY7m6JsbfCFozItMED\n" +
+				"N0eEHqCK/BNWRzf9lc7bNSrDMU3CgLq2MNzyN3U+9JLvTiG2Rc7k99+kD/JQ1Eqe\n" +
+				"qk77h8cuMwKBgQCcHq1ShMHJkqkyABwEjyttSEg9WKaeuAIyDBJtiegZNY4yEc00\n" +
+				"+B/l7hcPEABQGVVc2s2U6ANCblITCOvg/jRkis1q9siRBzf6Qa/0WgEJOJBgikRp\n" +
+				"COFHZ1gMlRlA9X75r7gRO6/3NKxahHVVJ54TlDiUEayj4h9qwbhw9teNeQKBgFY2\n" +
+				"oOKQTVaWXJOrmZDuIDG5NCnwxIwzGl+L/KBBhyfl7iuNmVA6ay+ogNTECu2hQM2V\n" +
+				"Nv4IgAxgPqOGr58ZlYdMli8toi/+XY0iII/YRrUgkgB68LxE6WQhw5TsCkapaYh6\n" +
+				"yLtm+sV3369P9huDDBqUpVCMQNZf7zA4c2JGMnHrAoGAKU9VfnS+wyyH8xOu1XG1\n" +
+				"C0c19H3DNQa/SopiT4Dr+dXcVpLGDg/4RKh1hFbfsXaH7WiPSr+1AQW+syyXT/sH\n" +
+				"pa0s4bD3IdogBj4r2yoAdvgSebE5+9EBl46s0S2TY8Gvc6+/ExQBEPUvid4XW57z\n" +
+				"GeWpOx+foGRV/csMvxNlMwg=\n" +
+				"-----END PRIVATE KEY-----\n";
 
 
 
-		String clientId = "asf2CaB2lu";
-		String clientSecret = "U23coC";
-		String serviceAccount = "serviceaccount@mail.workslife.kr";
-		String scope = "audit audit.read board board.read bot bot.read calendar calendar.read contact contact.read directory directory.read file file.read group group.folder group.folder.read group.read mail mail.read orgunit orgunit.read user user.read";
+		String clientId = "vcLxZndZyYDwtBuTvxuC";
+		String clientSecret = "MXmguprU1i";
+		String serviceAccount = "a7vcu.serviceaccount@danal3.by-works.com";
+		String scope = "audit audit.read board board.read bot bot.read calendar calendar.read contact contact.read directory directory.read group group.read orgunit orgunit.read user user.read";
 
 		TokenInfo tokenMap = jwtService.getServerToken(serverPrivateKey, clientId, clientSecret, serviceAccount, scope);
 		assertThat(tokenMap).isNotNull();
